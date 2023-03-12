bc4452c6911ca608de5f498b65aa2383592cee52
Yegor Shabanov <yegorshabanov@Yegors-MBP-2.lan>
2023-03-12 16:19:39 -0700

    Updates

---
+++ precache-manifest.aeb64f2144c36ab48acba3ba24dd4f3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a409105b7dcd6bde701b",
+    "url": "/vehiclesite/static/css/main.cea4141a.chunk.css"
+  },
+  {
+    "revision": "a409105b7dcd6bde701b",
+    "url": "/vehiclesite/static/js/main.a409105b.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/vehiclesite/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "7bfcc10ed152119acd56",
+    "url": "/vehiclesite/static/js/2.7bfcc10e.chunk.js"
+  },
+  {
+    "revision": "12b5c2abd5eb12cc7a0a",
+    "url": "/vehiclesite/static/js/runtime~main.12b5c2ab.js"
+  },
+  {
+    "revision": "3daf9b76eb034b7f3dec779c8222ed07",
+    "url": "/vehiclesite/index.html"
+  }
+];
